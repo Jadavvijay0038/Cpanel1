469ffb1fa17fe4f1efb745817fd16eeacf69e928
VJ <jadavvijay0@gmail.com>
2023-02-14 22:03:12 +0530

    'jsdvn'

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent {
+  RegistrationForm: FormGroup;
+  constructor() {
+    this.RegistrationForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      mobile: new FormControl('', [Validators.required, Validators.pattern('[0-9]+')]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      confirm: new FormControl('', [Validators.required]),
+      dob: new FormControl('', [Validators.required]),
+      gender: new FormControl('', [Validators.required]),
+      hobbies: new FormControl('')
+    });
+  }
 
+  onSubmit(){
+    if (this.RegistrationForm.valid) {
+      console.log(this.RegistrationForm.value);
+    }
+  }
 }
